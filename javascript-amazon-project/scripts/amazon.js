ecc36a8550b0063a74570c14094921645957b218
Kelvin Sim <kelvinsim2408@gmail.com>
2025-06-09 11:54:49 +0800

    Use products.js file

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -1,29 +1,4 @@
-// Save the Data (Data Structure)
-const products = [{
-    images: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090
-}, {
-    images: "images/products/intermediate-composite-basketball.jpg",
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095
-}, {
-    images: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799
-}];
+
 
 // Combine HTML together
 let productsHTML = '';
@@ -31,10 +6,10 @@ let productsHTML = '';
 
 // Generate the HTML 
 products.forEach((product) => {
-    productsHTML += `
+  productsHTML += `
     <div class="product-container">
         <div class="product-image-container">
-          <img class="product-image" src="${product.images}">
+          <img class="product-image" src="${product.image}">
         </div>
 
         <div class="product-name limit-text-to-2-lines">
